54bd23d7da565478b4b4907b609effb8eb03d349
clwu1119 <zhenglongwu@gmail.com>
2026-01-02 21:31:19 +0700

    Add expat-flow.html to service worker cache
    Updated service worker to include new file in cache.

--- sw.js
+++ sw.js
@@ -1,14 +1,33 @@
-// 簡單的 Service Worker，讓工具箱可以離線開啟
+const CACHE_NAME = 'toolbox-v2'; // 升級版本號，強迫瀏覽器更新快取
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './meeting-pro.html',
+  './team-calendar.html',
+  './wujia.html',
+  './expat-flow.html',  // 新增這一行
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('toolbox-v1').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './meeting-pro.html',
-      './team-calendar.html',
-      './wujia.html'
-    ]))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting(); // 強制立即啟用新版 SW
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key); // 刪除舊版本快取
+        }
+      }));
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
